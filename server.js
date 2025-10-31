51bf7c49b7215bbd6e822c3347bbc2708e9a42a3
bijay-develops <bijaybk307@gmail.com>
2025-10-31 12:01:24 +0545

    Initial commit: Set up Express server with EJS view engine and created basic pad view.

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+//setting view engine to ejs
+app.set('view engine', 'ejs');
+
+//public folder to store assets
+app.use(express.static(__dirname + '/public'));
+
+//routes for app
+app.get('/', function(request, response){
+    response.render('pad');
+});
+
+// listen on port 8000 ( for localhost ) or the port defined by the environment
+var port = process.env.PORT || 8000;
+app.listen(port, function(){
+    console.log('Server is running on port ' + port);
+});
