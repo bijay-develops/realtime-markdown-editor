d26faaa65f2bcb9b3e830c3309535f6b0bfd881d
bijay-develops <bijaybk307@gmail.com>
2025-11-08 00:09:01 +0545

    Enhance script.js to handle tab key input in the textarea and improve markdown conversion logic; update pad.ejs to correct stylesheet paths and script inclusions.

--- server.js
+++ server.js
@@ -1,35 +1,32 @@
 var express = require('express');
-var app = express();
+    var app = express();
 
-//setting view engine to ejs
-app.set('view engine', 'ejs');
+    // set the view engine to ejs
+    app.set('view engine', 'ejs');
 
-//public folder to store assets
-app.use(express.static(__dirname + '/public'));
+    // public folder to store assets
+    app.use(express.static(__dirname + '/public'));
 
-//routes for app
-app.get('/', function(req, res){
-    res.render('pad');
-});
+    // routes for app
+    app.get('/', function(req, res) {
+      res.render('pad');
+    });
+    app.get('/(:id)', function(req, res) {
+      res.render('pad');
+    });
 
-app.get('/(:id)', function(req, res){
-    res.render('pad');
-});
+    // get sharejs dependencies
+    var sharejs = require('share');
+    require('redis');
 
-// get sharejs dependencies
-var sharejs = require ('share');
-require('redis');
+    // options for sharejs
+    var options = {
+      db: {type: 'redis'},
+    };
 
-//options for sharejs
-var options = {
-    db: {type: 'redis'},
-};
+    // attach the express server to sharejs
+    sharejs.server.attach(app, options);
 
-//attach the expressjs server to sharejs
-sharejs.server.attach(app, options);
-
-// listen on port 8000 ( for localhost ) or the port defined by the environment
-var port = process.env.PORT || 8002;
-app.listen(port, function(){
-    console.log('Server is running on port ' + port);
-});
+    // listen on port 8000 (for localhost) or the port defined for heroku
+    var port = process.env.PORT || 8000;
+    app.listen(port);
